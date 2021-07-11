e170729d305cbf06f04b3c9e00224398cfc4e3b8
ArzunDhz <86089364+ArzunDhz@users.noreply.github.com>
2021-07-11 13:20:12 +0545

    nv

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
 
-var a = prompt("your name");
-var b = prompt("next name");
+
+
+
+$("button").on("click",animate);
+
+
+
+
+
+
+
+
+
+
+function animate(){
+  $(".enter").addClass("pressed")
+  setTimeout(function(){
+            $(".enter").removeClass('pressed');
+    }, 100);
+    onEnter();
+}
+
+function onEnter(){
+
+
+
+
 
 var random = Math.floor(Math.random()*100+1);
 
 
 
-document.querySelectorAll("p")[0].innerHTML = a ;
+var a = prompt("Enter your name");
+var b = prompt("Enter lover name");
 
-document.querySelectorAll("p")[1].innerHTML = b ;
-
-
-document.querySelectorAll("p")[2].innerHTML = random+"%" ;
-
-var heart = document.querySelector("img");
+$("p.your").html(a)
+$("p.another").html(b)
+$("p.percentage").html(random+"%");
 
 
-
-
-
+$(".link").attr("href","https://www.youtube.com/")
 
 
 var image = document.querySelector("img");
-sourceImg1 ="images/"+ "heart1"+".png" ;
-sourceImg2 ="images/"+ "heart2"+".png" ;
+sourceImg1 ="images/heart1.png";
+sourceImg2 ="images/heart2.png";
+
+
 if(random<50){
-  image.setAttribute("src" , sourceImg2);
-  document.querySelectorAll("p")[3].innerHTML = "its fine " ;
+  $("img").attr("src" , sourceImg2)
+  $("p.Remarks").html("its fine ")
+  var audio1 = new Audio('sounds/cry.mp3');
+audio1.play();
+
 }
-else{
-  image.setAttribute("src" , sourceImg1);
-  document.querySelectorAll("p")[3].innerHTML = " Number doesn't matter" ;
+else {
+  $("img").attr("src" , sourceImg1)
+  $("p.Remarks").html(" Number doesn't matter")
+  var audio2 = new Audio('sounds/laugh.mp3');
+  audio2.play();
+
+}
+
+
 }
